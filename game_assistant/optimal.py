7632f330f7a924edf7be0c3c2e59b290d3fcd467
gabrielbuffet <gabriel-buffet@hotmail.fr>
2025-07-30 00:02:45 +0200

    Fix optimum calculation

--- game_assistant/optimal.py
+++ game_assistant/optimal.py
@@ -4,17 +4,6 @@ from typing import Optional
 from game_assistant.models import Instance
 
 def solve_instance(instance: Instance, forbidden_routes: Optional[set] = set()) -> dict[str, dict[str, int]]:
-    """
-    Solve the instance using linear programming to find the optimal routes between villages.
-    Args:
-        instance (Instance): The instance containing villages and their routes.
-        forbidden_routes (set): A set of tuples representing forbidden routes.
-    Returns:
-        Dict[str, Dict[str, int]]: A dictionary where keys are village names and values
-        are dictionaries of routes with amounts.
-    Raises:
-        ValueError: If the instance has no villages or routes.
-    """
     if not instance.villages:
         raise ValueError("Instance has no villages.")
     
@@ -41,18 +30,33 @@ def solve_instance(instance: Instance, forbidden_routes: Optional[set] = set()) 
     
     problem += lpSum(x[i, j] for i, j in allowed_routes), "Total_Routes"
 
+    # for i, village in enumerate(instance.villages):
+    #     if village.production >= 0:
+    #         problem += lpSum(x[i, j] for j in range(n) if (i, j) in allowed_routes) <= village.production, f"Production_Constraint_{i}"
+    #     else:
+    #         problem += lpSum(x[j, i] for j in range(n) if (j, i) in allowed_routes) >= -village.production, f"Consumption_Constraint_{i}"
+
     for i, village in enumerate(instance.villages):
-        if village.production >= 0:
-            problem += lpSum(x[i, j] for j in range(n) if (i, j) in allowed_routes) <= village.production, f"Production_Constraint_{i}"
+        inflow = lpSum(x[j, i] for j in range(n) if (j, i) in allowed_routes)
+        outflow = lpSum(x[i, j] for j in range(n) if (i, j) in allowed_routes)
+        if village.production > 0:
+            problem += outflow <= village.production, f"Production_Constraint_{i}"
+            problem += inflow == 0, f"Inflows_Zero_{i}"
+        elif village.production < 0:
+            problem += inflow == -village.production, f"Consumption_Constraint_{i}"
+            problem += outflow == 0, f"Outflows_Zero_{i}"
         else:
-            problem += lpSum(x[j, i] for j in range(n) if (j, i) in allowed_routes) >= -village.production, f"Consumption_Constraint_{i}"
+            problem += inflow == outflow, f"Balance_Constraint_{i}"
+        
 
-    for (i, j), var in x.items():
-        var.setInitialValue(instance.routes_matrix[i, j])
+
+
+    # for (i, j), var in x.items():
+    #     var.setInitialValue(instance.routes_matrix[i, j])
     
-    solver = PULP_CBC_CMD(msg=False, warmStart=True)
-    problem.solve(solver)
-
+    # solver = PULP_CBC_CMD(msg=False, warmStart=True)
+    # problem.solve(solver)
+    problem.solve()
     status = LpStatus[problem.status]
     if status != 'Optimal':
         raise ValueError(f"Problem status is not optimal: {status}")
